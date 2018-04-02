d7122413fccf674feba741ba87624915da9dd27f
Katia Romaniuk <katia-l@mail.ru>
2018-04-02 22:59:25 +0300

    added karma-coverage

--- karma.conf.js
+++ karma.conf.js
@@ -9,8 +9,11 @@ module.exports = function(config) {
         'node_modules/@uirouter/angularjs/release/angular-ui-router.js',
         'node_modules/angular-mocks/angular-mocks.js',
         'public/*.js',
+        'tests/*.js',
         // 'view*/**/*.js'
       ],
+
+      browsers: ['Chrome'],
   
       autoWatch: true,
   
@@ -21,7 +24,21 @@ module.exports = function(config) {
         'karma-jasmine',
         'karma-junit-reporter'
       ],
-  
+
+      reporters: ['progress', 'coverage'],
+
+      preprocessors: {
+        // source files, that you wanna generate coverage for
+        // do not include tests or libraries
+        // (these files will be instrumented by Istanbul)
+        'public/*.js': ['coverage']
+      },
+
+      coverageReporter: {
+        type : 'html',
+        dir : 'coverage/'
+      },
+
       junitReporter: {
         outputFile: 'test_out/unit.xml',
         suite: 'unit'
